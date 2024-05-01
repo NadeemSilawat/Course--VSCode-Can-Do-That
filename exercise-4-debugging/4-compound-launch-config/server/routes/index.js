4d0c7e82645ca29b0c164bbcf807971f0edf8ba0
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-01 19:03:35 +0530

    Added nodemon exercise

---
+++ exercise-4-debugging/4-compound-launch-config/server/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+const API_BASE = process.env.API_BASE;
+
+router.get('/setColor', async (req, res, next) => {
+    const color = req.query.color;
+
+    console.log(`The Color is ${color}`);
+
+    // call the API to set the lamp color
+    try {
+        await axios.get(`${API_BASE}/setColor?color=${color}`);
+        res.json({ color: color });
+    } catch (err) {
+        res.status = 500;
+    }
+});
+
+module.exports = router;
